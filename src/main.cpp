f4974979425048a9ce226a20e14fec8276d7623a
huaweil <huaweil@nvidia.com>
2022-01-21 17:22:53 +0800

    test review

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
 using namespace std;
 
+void testFunc()
+{
+    int a = 1;
+    int b = 2;
+    int c = 3;
+    std::cout << a << " " << b << " " << c << " " << std::endl;
+    std::cout << " this is a reveiw workflow test";
+}
+
 int main(int argc, char** argv)
 {
     cout << "start" << endl;
